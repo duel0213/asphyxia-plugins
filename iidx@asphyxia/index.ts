11cbbb9e7ac05467d1d88af575fa2b3c40e39606
duel0213 <duel0213@users.noreply.github.com>
2025-02-08 20:10:21 +0900

    IIDX: Fixed where unable to login after event play (copula)

--- iidx@asphyxia/index.ts
+++ iidx@asphyxia/index.ts
@@ -255,7 +255,7 @@ export function register() {
     default: 9,
   });
   R.Config("pd_boss", {
-    name: "Event Phase (SP)",
+    name: "Event Phase (PD)",
     desc: "Chrono Seeker / Qpronicle Chord / PENDUAL TALISMAN",
     type: "integer",
     default: 3,
